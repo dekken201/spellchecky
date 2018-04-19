b5a02ea31bf3818e192f31408668b1d7b1b37aa1
dekken201 <lucksbp@gmail.com>
2018-04-18 22:17:57 -0400

    partial creation of words lists by number of characters, and partial creation of spell checking

--- core/functions.py
+++ core/functions.py
@@ -1,7 +1,9 @@
-import json
-import os
+import json, os
+from fuzzywuzzy import fuzz
+
 
 basepath = os.path.dirname(__file__)
+listspath = os.path.join(basepath,"..","lists\\")
 
 def load_words(filename):#nome do arquivo, com sua extensão
     try:
@@ -12,6 +14,30 @@ def load_words(filename):#nome do arquivo, com sua extensão
     except Exception as e:
         return str(e)
 
+def separate(dictionary):
+    for word in dictionary:
+        filename = str(len(word))+" words.txt"
+        try:
+            with open(listspath+filename,'a+') as f:
+                f.write(word+"\n")
+        except Exception as e:
+            return str(e)
 
-english_words = load_words("english_dict.json")
-print(english_words["fate"])
+def test(word):
+    filename = str(len(word)) + " words.txt"
+    maiorRatio = 0
+    bestMatch = ""
+    try:
+        with open(listspath + filename, 'r') as f:
+            for line in f:
+                ratio = fuzz.ratio(word,line)
+                if (ratio > maiorRatio):
+                    bestMatch = [line,ratio]
+            print(bestMatch)
+    except Exception as e:
+        return str(e)
+
+#TO GENERATE THE LISTS BY NUMBER OF WORDS
+#separate(load_words("english_dict.json"))
+
+test("aaaaaa")
