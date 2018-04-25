6b140638525982f14a4e0d3c8258d1bb6f1d59a0
dekken201 <lucksbp@gmail.com>
2018-04-24 20:49:57 -0400

    minor changes in names and better readme

--- core/run.py
+++ core/run.py
@@ -3,13 +3,14 @@ from core.functions import *
 #LOADS THE JSON DICTIONARY, CREATES A TREE, ADDS THE DICTIONARY TO THE TREE, AND DUMPS IT INTO A PICKLE OBJECT
 #pickle_dump(BKTree(load_words("english_dict")),"english_dict_pickle")
 
-def main():
+def run():
     tree = pickle_load("english_dict_pickle")
     while True:
         user_input = input("Write the word you want spellchecked:")
         if user_input != "":
-            print(tree.find(user_input, 1))
+            print(tree.find(user_input, 3))
         else:
             break
 
-main()
+if __name__ == "__main__":
+    run()
